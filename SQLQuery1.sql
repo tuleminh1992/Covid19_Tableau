3f79f0a9371e21b04bbf175c02b281b692d2689f
tuleminh1992 <63758702+tuleminh1992@users.noreply.github.com>
2021-12-12 01:22:20 -0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,32 @@
+-- 1. 
+Select	sum(new_cases) as total_cases, 
+		SUM(cast(new_deaths as int)) as total_deaths,
+		sum(cast(new_deaths as int))/sum(new_cases) * 100 as Death_Percentage
+FROM dbo.CovidDeaths$
+where continent is null
+order by 1,2
+
+--2. 
+Select location, Sum(cast(new_deaths as int)) as Total_Death_Count
+From CovidDeaths$
+Where continent is null
+and location not in ('World', 'European Union', 'International')
+Group by Location
+order by 2 desc
+
+-- 3. 
+Select Location, Population, Max(total_cases) as Highest_Infection_Count, Max((total_cases/population)) *100 as Percent_Population_Infected
+From CovidDeaths$
+Group by Location, Population
+order by 4 desc
+
+--4.
+Select Location, Population,  cast(Date as Date) as Date ,Max(total_cases) as Highest_Infection_Count, Max((total_cases/population)) *100 as Percent_Population_Infected
+From CovidDeaths$
+Group by Location, Population, Date
+order by 5 desc
+
+
+
+
+
